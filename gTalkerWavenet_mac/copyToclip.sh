44b73b95a99183923392a75af403a28d6286fa9f
ccosnett <conorcosnett@gmail.com>
2022-06-22 15:15:51 +0100

    removed git rm mac

--- gTalkerWavenet_mac/copyToclip.sh
+++
@@ -1,4 +0,0 @@
-# copy this to clipboard and then evaluate on command line
-
-export GOOGLE_APPLICATION_CREDENTIALS="/home/conor/Dropbox/05_PROGRAMS/24_chrome_interface_modding/02_attempt_2/credential.json"
-
--- gTalkerWavenet_mac/pythonScript.py
+++
@@ -1,5 +0,0 @@
-#!/home/conor/anaconda3/envs/py383/bin/python
-
-import sys
-first_arg=sys.argv[1]
-print(first_arg)
